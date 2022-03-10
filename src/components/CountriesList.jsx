d9eca01f6c4e0ab0b7d382b027bf30aaecf537a7
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-03-10 19:23:16 +0100

    Countries

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios"
+import { Link } from 'react-router-dom'
+
+
+function CountriesList() {
+
+  const [listOfCountries, setListOfCountries] = useState([])
+
+  const getCountries = async () => {
+    const response = await axios.get("https://ih-countries-api.herokuapp.com/countries")
+    //console.log(response.data);
+    
+    setListOfCountries(response.data)
+  }
+
+  useEffect(() => {
+    getCountries()
+
+  }, [])
+
+  if (!listOfCountries) {
+    return <div>...Loading</div>;
+    
+  }
+
+  return (
+    <div>
+    
+    <h2>Countries List</h2>
+    
+    {listOfCountries.map((eachCountry, index) => {
+        
+        return (
+          <div key={index + eachCountry.name.common}>
+          <Link to={`/countries/${eachCountry.name.common}`}>{eachCountry.name.common}</Link>
+          </div>
+        )
+      })}
+    
+    </div>
+  )
+}
+
+export default CountriesList
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    
+    <div>
+      <Link to='/'>LAB - WikiCountries</Link>
+
+    </div>
+
+  )
+}
+
+export default Navbar
